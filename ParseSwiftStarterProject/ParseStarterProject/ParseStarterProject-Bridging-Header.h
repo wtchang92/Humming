949fae135ac751d69689402aaedf9ef626295164
someotherdata <yizhang@Yis-MacBook-Pro.local>
2015-05-02 16:29:25 -0400

    Master Copy (Pushing Update)

--- ParseSwiftStarterProject/ParseStarterProject/ParseStarterProject-Bridging-Header.h
+++ ParseSwiftStarterProject/ParseStarterProject/ParseStarterProject-Bridging-Header.h
@@ -4,10 +4,9 @@
 //  Copyright 2011-present Parse Inc. All rights reserved.
 //
 
-#ifndef ParseStarterProject_Bridging_Header_h
-#define ParseStarterProject_Bridging_Header_h
+#ifndef SpotifyDJ_SpotifyDJ_Bridging_Header_h
+#define SpotifyDJ_SpotifyDJ_Bridging_Header_h
 
-// If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
-//#import <ParseFacebookUtils/PFFacebookUtils.h>
+#import <Spotify/Spotify.h>
 
 #endif
